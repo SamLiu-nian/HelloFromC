44d0689c835abfdc195180c018f11d4504a36a4d
SamLiu-nian <nliu4986@uni.sydney.edu.au>
2020-02-26 16:28:14 +1100

    First Commit

---
+++ HelloFormC/HelloFormC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloFormC
+//
+//  Created by 刘年 on 26/2/20.
+//  Copyright © 2020 刘年. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello from C!\n");
+    return 0;
+}
